0f0b8791a26090f28fd3f123051e0dbfecc008f8
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-09-07 21:37:03 +0530

    talent leaderboard

--- js/google-sheets-html.js
+++ js/google-sheets-html.js
@@ -118,12 +118,17 @@ function rowsCount(item){
      $('#conDiv').append('<div class="LeaderStrip"><div class="position">'+(counter)+'</div><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass" /></a></div><div class="lastDiv"><span class="dancing">'+times+'</span></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal" />'+name+'</div></div>');
 
   }
-else  if(counter>=5  &&  counter<12){
-
-
-  
-
+else  if(counter>=5  &&  counter<16){
   $('#conDivTalent').append('<div class="LeaderStrip"><div class="position">'+(counter1)+'</div><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass" /></a></div><div class="lastDiv"><span class="dancing">'+times+'</span></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal" />'+name+'</div></div>');
+
+  counter1++;
+}else  if(counter>=16  &&  counter<22){
+  $('#conDivTalent2').append('<div class="LeaderStrip"><div class="position">'+(counter1)+'</div><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass" /></a></div><div class="lastDiv"><span class="dancing">'+times+'</span></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal" />'+name+'</div></div>');
+
+  counter1++;
+}
+else  if(counter>=22  &&  counter<27){
+  $('#conDivTalent3').append('<div class="LeaderStrip"><div class="position">'+(counter1)+'</div><div><a href="http://www.kktv1.com/m/?roomid='+id+'"><img src="../common/img/kk_head_avatar_men.png" id="u_img_'+id+'" alt="" class="imgClass" /></a></div><div class="lastDiv"><span class="dancing">'+times+'</span></div><div class="centerDiv"><img src="img/coin.png" class="imgsmal" />'+name+'</div></div>');
 
   counter1++;
 }
